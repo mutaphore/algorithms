efce428a1bd38b992c6f24b97b750f12e72fa8cc
Dewei <deweichen47@gmail.com>
2015-02-01 19:15:42 -0500

    fixed heap sort issue

--- heap.py
+++ heap.py
@@ -30,11 +30,15 @@ class Heap(object):
 
 
     def __repr__(self):
-        return repr(self._heap)
+        return repr(self._heap[:self.heap_size])
 
 
     def __str__(self):
-        return str(self._heap)
+        return str(self._heap[:self.heap_size])
+
+
+    def to_arr(self):
+        return self._heap
 
 
 def max_heapify(A, i):
@@ -58,33 +62,53 @@ def build_max_heap(A):
 
 
 def heap_sort(A):
-    """Sorts an array into increasing order"""
+    """Sorts an array into increasing order by using the heap data structure"""
+    A = Heap(A)
     build_max_heap(A)
-    for i in reversed(range(1, len(A) + 1)):
+    for i in reversed(range(2, len(A) + 1)):
         A[1], A[i] = A[i], A[1]
         A.heap_size -= 1
         max_heapify(A, 1)
+    return A.to_arr()
 
 
 def heap_max(A):
+    """Return maximum element of a max heap"""
     return A[1]
 
 
 def heap_extract_max(A):
+    """Extracts the max element from a max heap and also return it"""
     if A.heap_size < 1:
         raise IndexError
     heap_max = A[1]
     A[1] = A[A.heap_size]
     A.heap_size -= 1
+    max_heapify(A, 1)
     return heap_max
+
+
+def heap_increase_key(A, i, key):
+    """Increase the value of element at index i to key"""
+    if A[i] > key:
+        return        
+    A[i] = key 
+    while i / 2 > 1 and A[i/2] < A[i]:
+        A[i/2], A[i] = A[i], A[i/2]
+        i /= 2
+
 
 if __name__ == "__main__":
     heap = Heap([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
     build_max_heap(heap)
     print "Max heap %r" % heap
-    print "Heap max is %r" % heap_max(heap)
-    
-#    heap_sort(heap)
-#    print "Sorted array %r" % heap
+    print "Extract heap max %r" % heap_extract_max(heap) 
+    print "Max heap %r" % heap
+    heap_increase_key(heap, 8, 11)
+    print "Heap increase key %r" % heap
+
+    arr = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+    sorted_arr = heap_sort(arr)
+    print "Sorted array %r" % sorted_arr
 
 
